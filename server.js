42b6fd61684357f271e81aa74a34186f1b756e32
jinouwuque <ee2win@gmail.com>
2017-04-08 22:18:41 -0400

    create some render mechanism

--- server.js
+++ server.js
@@ -6,16 +6,14 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.set('x-powered-by', false);
-app.set('view engine', 'jade');
-app.set('views', './views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.get('/', (req,res) => {
-  res.render('index', {
-    title: "test123"
+  res.sendFile('./rendered/main.html',{
+    root: __dirname
   });
 });
+
 
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", server_port " + server_port + '...' );
---
+++ server/render/main.js
@@ -0,0 +1,23 @@
+var fs = require('fs'),
+  jade = require('jade');
+
+var configJson = null;
+try {
+  configJson = JSON.parse(fs.readFileSync('../config.json'));
+}
+
+catch(err){
+  console.error("JSON Error: " + err.message);
+}
+
+try {
+  var html = jade.renderFile('../../views/main/index.jade', configJson);
+  var filePwd = '../../rendered/main.html';
+  fs.openSync(filePwd, 'w');
+  fs.writeFileSync(filePwd, html);
+}
+catch(err){
+  console.error("Jade Build Error: " + err.message);
+}
+
+console.log("render success!")
