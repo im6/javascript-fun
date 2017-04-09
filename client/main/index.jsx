8ca81c93d67dedf6c081f97a13a908136317f8bc
jinouwuque <ee2win@gmail.com>
2017-04-09 10:46:08 -0400

    add type effect

--- client/main/index.jsx
+++ client/main/index.jsx
@@ -1,4 +1,32 @@
 import 'style-loader!css-loader!purecss/build/pure-min.css';
 import './style.less';
+import './typed.less';
 
-console.log('js loaded.');
+import '../assets/typed.min.js';
+const TYPECOLOR = [
+  "#ffefbc",
+  "#95E1D3",
+  "#FCE38A",
+  "#F38181",
+  "#EAFFD0",
+  "#adc1f9",
+  "#DDE8B9",
+  "#F16B6F",
+  "#E4F5E5",
+  "#FF847B",
+  "#88A6E5"
+];
+
+document.addEventListener("DOMContentLoaded", () =>{
+  let typeElem = document.getElementsByClassName('type')[0],
+    COLORCOUNT = TYPECOLOR.length;
+
+  window.Typed.new(".type", {
+    stringsElement: document.getElementById('typed-strings'),
+    typeSpeed: 60,
+    loop: true,
+    preStringTyped: (index)=> {
+      typeElem.style.color = TYPECOLOR[index% COLORCOUNT];
+    },
+  });
+});
