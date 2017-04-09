ae185be82696440bc6f3f081f9dfc918c7d4b469
jinouwuque <ee2win@gmail.com>
2017-04-09 10:03:38 -0400

    update the layout style

--- client/main/index.jsx
+++ client/main/index.jsx
@@ -1,5 +1,4 @@
 import 'style-loader!css-loader!purecss/build/pure-min.css';
-import 'style-loader!css-loader!purecss/build/grids-responsive-min.css';
 import './style.less';
 
 console.log('js loaded.');
