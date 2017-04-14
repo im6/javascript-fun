6c550b623ef51f9d0e1c373272bf3b2b870a397c
jinouwuque <ee2win@gmail.com>
2017-04-13 23:27:36 -0400

    minor change on style

--- server/crawler/github.js
+++ server/crawler/github.js
@@ -6,51 +6,73 @@ let request = require('request'),
   fs = require('fs');
 
 const CONCURRENCY = 4,
-  JSONPATH = '../data/db.json',
-  getNum = (obj, cb)=> {
-  let url = obj.git;
-  request({
-    method: 'GET',
-    uri: 'https://github.com'+url,
+  JSONPATH = '../data/db.json';
 
-  }, function(err, res, body){
-    if(err){
-      console.error(err);
-      cb(true, err);
-    }else{
-      try{
-        let $ = cheerio.load(body);
-        let elems = $('.social-count.js-social-count');
-        let starElem = elems[0];
-        let num = starElem.children[0].data.trim();
-        obj['star'] = num;
-        cb(null, obj);
-      } catch (ex){
-        console.error(ex);
-        cb(true, ex);
+const privateFn = {
+  getNum: (obj, cb)=> {
+    let url = obj.git;
+    request({
+      method: 'GET',
+      uri: 'https://github.com' + url,
+    }, function(err, res, body){
+      if(err){
+        console.error(err);
+        cb(true, err);
+      }else{
+        try{
+          let $ = cheerio.load(body);
+          let elems = $('.social-count.js-social-count');
+          let starElem = elems[0];
+          let num = starElem.children[0].data.trim();
+          obj['star'] = num;
+          cb(null, obj);
+        } catch (ex){
+          console.error(ex);
+          cb(true, ex);
+        }
       }
+    });
+  },
+
+  getJson:(path) => {
+    var db = null;
+    try {
+      db = JSON.parse(fs.readFileSync(path));
     }
-  });
+
+    catch(err){
+      console.error("JSON Error: " + err.message);
+    }
+
+    return db;
+  },
+};
+
+const publicFn = {
+  start: () => {
+    let db = privateFn.getJson(JSONPATH);
+    const deferred = new Promise((resolve, reject) => {
+      async.mapLimit(db.package, CONCURRENCY, (v, cb) => {
+        console.log(`crawling ${v.name}...`);
+        privateFn.getNum(v, cb);
+      }, (error, data) => {
+        if(error){
+          console.error(error);
+          reject(error);
+        }else{
+          resolve(data);
+        }
+      });
+    });
+
+    return deferred;
+  }
 };
 
 
-var db = null;
-try {
-  db = JSON.parse(fs.readFileSync(JSONPATH));
-}
-
-catch(err){
-  console.error("JSON Error: " + err.message);
-}
 
 
-async.mapLimit(db.package, CONCURRENCY, (v, cb) => {
-  console.log(`crawling ${v.name}...`);
-  getNum(v, cb);
-}, (error, data) => {
-  if(error){
-    console.error(error);
-  }else{
-    console.log(data);
-  }
-});
+
+
+
+module.exports = publicFn;
