eb2bba744665f176eb3feeac0821020d3d084305
jinouwuque <ee2win@gmail.com>
2017-04-13 21:37:48 -0400

    fix the crawler

--- server/crawler/github.js
+++ server/crawler/github.js
@@ -1,64 +1,56 @@
-
 "use strict";
+
 let request = require('request'),
   cheerio = require('cheerio'),
   async = require('async'),
   fs = require('fs');
 
-let getGitStar = (url) => {
-  let deferred = new Promise((resolve, reject) => {
-    request({
-      method: 'GET',
-      uri: 'https://github.com'+url,
+const CONCURRENCY = 4,
+  JSONPATH = '../data/db.json',
+  getNum = (obj, cb)=> {
+  let url = obj.git;
+  request({
+    method: 'GET',
+    uri: 'https://github.com'+url,
 
-    }, function(err, res, body){
-      if(err){
-        console.error(err);
-        reject(err);
-      }else{
-        try{
-          let $ = cheerio.load(body);
-          let elems = $('.social-count.js-social-count');
-          let starElem = elems[0];
-          let num = starElem.children[0].data.trim();
-          resolve(num);
-        } catch (ex){
-          reject(err);
-        }
+  }, function(err, res, body){
+    if(err){
+      console.error(err);
+      cb(true, err);
+    }else{
+      try{
+        let $ = cheerio.load(body);
+        let elems = $('.social-count.js-social-count');
+        let starElem = elems[0];
+        let num = starElem.children[0].data.trim();
+        obj['star'] = num;
+        cb(null, obj);
+      } catch (ex){
+        console.error(ex);
+        cb(true, ex);
       }
-    });
+    }
   });
-
-  return deferred;
 };
 
-//getGitStar('facebook/react').then((res)=> {
-//  debugger;
-//});
 
-
-
-
-var configJson = null;
+var db = null;
 try {
-  configJson = JSON.parse(fs.readFileSync('../config.json'));
+  db = JSON.parse(fs.readFileSync(JSONPATH));
 }
 
 catch(err){
   console.error("JSON Error: " + err.message);
 }
 
-let newLink = [];
-configJson.link.forEach((v1, k1)=>{
-  let promiseList = [];
-  v1.list.forEach(v => {
-    promiseList.push(getGitStar(v.git));
-  });
-  Promise.all(promiseList).then(values => {
-    values.forEach((v3, k3) => {
-      v1.list[k3].star = v3;
-    });
-  });
+
+async.mapLimit(db.package, CONCURRENCY, (v, cb) => {
+  console.log(`crawling ${v.name}...`);
+  getNum(v, cb);
+}, (error, data) => {
+  if(error){
+    console.error(error);
+  }else{
+    console.log(data);
+  }
 });
-
-
