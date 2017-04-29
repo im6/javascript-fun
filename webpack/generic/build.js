720ec42a92daa06105a6a8f2682347e25af62cde
jinouwuque <ee2win@gmail.com>
2017-04-29 11:13:49 -0400

    add build generic

--- webpack/generic/build.js
+++ webpack/generic/build.js
@@ -1,1 +1,58 @@
+"use strict";
+var webpack = require('webpack');
+var path = require('path');
 
+var webpackConfig = {
+  devtool: 'cheap-module-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            "presets": ["es2015"]
+          }
+        }]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ],
+        exclude: /node_modules/
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      mange:{
+        "screw-ie8" : true
+      },
+      compress : {
+        "screw_ie8" : true,
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+  ],
+  //entry: './client/modules/site/index.jsx',
+  //output: {
+  //  filename: 'bundle.js',
+  //  path: path.join(__dirname, '../public/site'),
+  //  publicPath: '/site',
+  //}
+};
+
+module.exports = webpackConfig;
--- webpack/main_build.js
+++ webpack/main_build.js
@@ -1,58 +1,14 @@
 "use strict";
-var webpack = require('webpack');
-var path = require('path');
+const path = require('path');
+let webpackConfig = require('./generic/build');
 
-var webpackConfig = {
-  devtool: 'cheap-module-source-map',
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            "presets": ["es2015"]
-          }
-        }]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader'
-        ],
-        exclude: /node_modules/
-      },
-    ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      mange:{
-        "screw-ie8" : true
-      },
-      compress : {
-        "screw_ie8" : true,
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    }),
-  ],
-  entry: './client/modules/main/index.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '../public/main'),
-    publicPath: '/main',
-  }
+const moduleName = 'main';
+
+webpackConfig.entry = `./client/modules/${moduleName}/index.jsx`;
+webpackConfig.output = {
+  filename: 'bundle.js',
+  path: path.join(__dirname, `../public/${moduleName}`),
+  publicPath: `/${moduleName}`,
 };
 
 module.exports = webpackConfig;
