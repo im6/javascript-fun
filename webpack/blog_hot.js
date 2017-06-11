5e8a8085f98eacb1406247220fe3153205a279d4
jinouwuque <ee2win@gmail.com>
2017-06-10 22:20:56 -0400

    add two more config

--- webpack/blog_hot.js
+++ webpack/blog_hot.js
@@ -1,60 +1,9 @@
 "use strict";
-var webpack = require('webpack');
-var path = require('path');
+let webpackConfig = require('./generic/hot');
 
-var webpackConfig = {
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            "presets": ["es2015"]
-          }
-        }]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader'
-        ],
-        exclude: /node_modules/
-      },
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("dev")
-      }
-    }),
-  ],
-  entry: './client/modules/blog/index.jsx',
-  output: {
-    filename: 'bundle.js',
-    publicPath: '/blog',
-  },
-  devServer: {
-    contentBase: "./rendered/blog/",  // set "public" path, relative to root
-    noInfo: true,
-    hot: true,
-    inline: true,
-    port: "3000",
-    host: "127.0.0.1",
-    historyApiFallback: {
-      index: "../rendered/blog/" // set "public" path, relative to __dirname
-    }
-  }
-};
+const moduleName = 'blog';
+webpackConfig.entry = `./client/modules/${moduleName}/index.jsx`;
+webpackConfig.output.publicPath = `/${moduleName}`;
+webpackConfig.devServer.historyApiFallback.index = `${moduleName}/`;
 
 module.exports = webpackConfig;
