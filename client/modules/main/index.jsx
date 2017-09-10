ac006fed5360aa127cfc1054eb53a728986d93db
jinouwuque <ee2win@gmail.com>
2017-09-09 21:59:32 -0400

    update the udpate text

--- client/modules/main/index.jsx
+++ client/modules/main/index.jsx
@@ -1,2 +1,9 @@
 import '../layout';
 import './style.less';
+
+document.addEventListener("DOMContentLoaded", () => {
+  let now = new Date();
+  let dt = now.toDateString();
+  const timeElem = document.getElementsByClassName('updateTxt')[0];
+  timeElem.innerText = `Last Update:   ${dt}, 0:00 AM`;
+});
