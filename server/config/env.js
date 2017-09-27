83c26bfe0aaebeca383c55fce345732f8eb48baa
jinouwuque <ee2win@gmail.com>
2017-09-26 21:28:56 -0400

    fix the dev server end

--- server/config/env.js
+++ server/config/env.js
@@ -4,6 +4,6 @@ const env = process.env;
 
 module.exports = {
   isDev: env.NODE_ENV === 'dev',
-  port: env.OPENSHIFT_NODEJS_PORT || 8080,
-  ip: env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+  port: env.PORT || 8080,
+  ip: 'localhost',
 };
