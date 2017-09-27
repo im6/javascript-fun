351b1ade12068f38ce23d8c9db3881dcbe10155e
jinouwuque <ee2win@gmail.com>
2017-09-26 22:25:15 -0400

    update db env config name

--- server/resource/mysqlConnection.js
+++ server/resource/mysqlConnection.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var pool = mysql.createPool({
   connectionLimit : 2,
-  host     : process.env['OPENSHIFT_MYSQL_DB_HOST'],
-  port     : process.env['OPENSHIFT_MYSQL_DB_PORT'],
+  host     : process.env['SQL_HOST'],
+  port     : process.env['SQL_PORT'],
   user     : process.env['SQL_USERNAME'],
   password : process.env['SQL_PASSWORD'],
   database : process.env['SQL_DATABASE']
