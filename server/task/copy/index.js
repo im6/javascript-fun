34bdb868d93c4671192879d888bb82a8cd25c3d9
im6 <ee2win@gmail.com>
2019-05-21 20:15:28 -0400

    update

--- server/task/copy/index.js
+++ server/task/copy/index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 const src = path.join(__dirname, '../../../public/');
 const dest = path.join(__dirname, '../../../../im6.github.io');
+const jsDest = path.join(__dirname, '../../../../im6.github.io/cdn/0/');
 
 const files = [
   'index.html',
@@ -14,17 +15,25 @@ const files = [
   'article/2/index.html',
   'article/3/index.html',
   'article/4/index.html',
+];
+
+const jsFiles = [
   'build/article0.js',
   'build/article1.js',
   'build/main.js',
   'build/site.js',
-];
+]
 
 const copy = (cb) => {
   console.log('start copy...');
   files.forEach(v => {
     console.log(`copying ${path.join(src, v)}`);
     fs.copyFileSync(path.join(src, v), path.join(dest, v));
+  });
+
+  jsFiles.forEach(v => {
+    console.log(`copying ${path.join(src, v)}`);
+    fs.copyFileSync(path.join(src, v), path.join(jsDest, v.replace(/(.)+\//, '')));
   });
   console.log('copy complete.');
   cb();
