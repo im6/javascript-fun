09ee172f99b46642b7d3f0a99450904b86d59e94
im6 <ee2win@gmail.com>
2020-02-24 19:05:53 -0500

    fix gitbox style

--- src/components/GitBox/index.jsx
+++ src/components/GitBox/index.jsx
@@ -5,16 +5,12 @@ import style from './style.less';
 const GitBox = ({ name, url, img, star }) => (
   <div className="pure-u-xl-1-5 pure-u-lg-1-4 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1-2">
     <div className={style.box}>
-      <div className={style.left}>
-        <img src={img} />
-      </div>
-      <div className={style.right}>
-        <h5>{name}</h5>
-        <div>
-          <a href={url} target="_blank">
-            &#9733; {star}
-          </a>
-        </div>
+      <img src={img} alt={name} />
+      <div className={style.rightText}>
+        <h3>{name}</h3>
+        <a href={url} target="_blank">
+          &#9733; {star}
+        </a>
       </div>
     </div>
   </div>
