11ab8ba7b66d7743168b5f36a3f985cbd0c18b8d
im6 <ee2win@gmail.com>
2020-02-28 21:53:16 -0500

    add env checker in collect task

--- src/collect/index.js
+++ src/collect/index.js
@@ -3,6 +3,11 @@ import collectGit from './task/git';
 import collectSite from './task/site';
 import { viewModelPath } from '../config';
 
+if (!process.env.SQL_HOST) {
+  console.error('DB connection info missing.'); // eslint-disable-line no-console
+  process.exit();
+}
+
 const gitOutputPath = path.join(process.cwd(), viewModelPath.git);
 const siteOutputPath = path.join(process.cwd(), viewModelPath.site);
 
