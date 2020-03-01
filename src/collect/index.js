4515a2779e4c45ce6153f35ab4fd5a9822912548
im6 <ee2win@gmail.com>
2020-03-01 01:19:07 -0500

    clean up and remove unused path and private symbol

--- src/collect/index.js
+++ src/collect/index.js
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import path from 'path';
 import collectGit from './task/git';
 import collectSite from './task/site';
 import { viewModelPath } from '../config';
@@ -9,14 +8,11 @@ if (!process.env.SQL_HOST) {
   process.exit();
 }
 
-const siteOutputPath = path.join(process.cwd(), viewModelPath.site);
-const gitOutputPath = path.join(process.cwd(), viewModelPath.git);
-
 collectSite((err, data) => {
   if (err) {
     console.log('site job failed!'); // eslint-disable-line no-console
   } else {
-    fs.writeFileSync(siteOutputPath, JSON.stringify(data));
+    fs.writeFileSync(viewModelPath.site, JSON.stringify(data));
     console.log('output site json successfully.'); // eslint-disable-line no-console
   }
 });
@@ -25,7 +21,7 @@ collectGit((err, data) => {
   if (err) {
     console.log('Github job failed!'); // eslint-disable-line no-console
   } else {
-    fs.writeFileSync(gitOutputPath, JSON.stringify(data));
+    fs.writeFileSync(viewModelPath.git, JSON.stringify(data));
     console.log('output github json successfully.'); // eslint-disable-line no-console
   }
 
--- src/collect/task/git.js
+++ src/collect/task/git.js
@@ -5,36 +5,34 @@ import orderBy from 'lodash.orderby';
 import getPackageList from './crawler';
 import sqlExecOne from '../mysqlConnection';
 
-const privateFn = {
-  convertGroupIcon: data => {
-    return data.reduce((accumulator, currentValue) => {
-      accumulator[`k${currentValue.id}`] = currentValue;
-      if (currentValue.icon.length === 0) {
-        accumulator[`k${currentValue.id}`].icon = null;
-      }
-      return accumulator;
-    }, {});
-  },
+const convertGroupIcon = data => {
+  return data.reduce((accumulator, currentValue) => {
+    accumulator[`k${currentValue.id}`] = currentValue;
+    if (currentValue.icon.length === 0) {
+      accumulator[`k${currentValue.id}`].icon = null;
+    }
+    return accumulator;
+  }, {});
+};
 
-  group: (data, iconMap) => {
-    const data1 = orderBy(data, v => numeral(v.star).value(), 'desc');
-    const data2 = groupBy(data1, 'group');
-    const data3 = Object.keys(data2);
-    const result = data3.map(k => {
-      const v = data2[k];
-      const newItem = iconMap[`k${k}`];
-      newItem.list = v;
-      if (newItem.icon) {
-        newItem.list.forEach((v1, k1) => {
-          if (!v1.img) {
-            newItem.list[k1].img = newItem.icon;
-          }
-        });
-      }
-      return newItem;
-    });
-    return orderBy(result, ['page', 'sort']);
-  },
+const group = (data, iconMap) => {
+  const data1 = orderBy(data, v => numeral(v.star).value(), 'desc');
+  const data2 = groupBy(data1, 'group');
+  const data3 = Object.keys(data2);
+  const result = data3.map(k => {
+    const v = data2[k];
+    const newItem = iconMap[`k${k}`];
+    newItem.list = v;
+    if (newItem.icon) {
+      newItem.list.forEach((v1, k1) => {
+        if (!v1.img) {
+          newItem.list[k1].img = newItem.icon;
+        }
+      });
+    }
+    return newItem;
+  });
+  return orderBy(result, ['page', 'sort']);
 };
 
 export default cb0 => {
@@ -51,8 +49,8 @@ export default cb0 => {
       if (err) {
         cb0(err);
       } else {
-        const iconMap = privateFn.convertGroupIcon(d0);
-        const data = privateFn.group(d1, iconMap);
+        const iconMap = convertGroupIcon(d0);
+        const data = group(d1, iconMap);
         cb0(null, data);
       }
     }
