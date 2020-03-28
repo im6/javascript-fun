0084cdf8897db4100f4adedd4257b2042aab259e
im6 <ee2win@gmail.com>
2020-03-28 00:09:15 -0400

    update minor check

--- src/collect/index.js
+++ src/collect/index.js
@@ -4,7 +4,11 @@ import collectGit from './task/git';
 import collectSite from './task/site';
 import { viewModelPath } from '../config';
 
-if (!process.env.SQL_HOST) {
+if (
+  !process.env.SQL_HOST ||
+  !process.env.SQL_USERNAME ||
+  !process.env.SQL_PASSWORD
+) {
   console.error('DB connection info missing.'); // eslint-disable-line no-console
   process.exit();
 }
