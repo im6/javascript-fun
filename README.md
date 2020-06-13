02ee01d3b663f4ebc7000ea589b53eb5cb01e67b
im6 <ee2win@gmail.com>
2020-06-13 16:24:33 -0400

    add screenshot

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Code generator for [javascript.fun](https://www.javascript.fun/)
 
+<p align="center">
+  <img width="950" height="623" src="https://github.com/im6/javascript-fun/blob/master/assets/screenshot.png" title="www.JavaScript.Fun">
+</p>
+
 ## Quick Start
 
 create `.env` file for DB connections.
--- cp.sh
+++ cp.sh
@@ -1,10 +1,19 @@
 im6_dir="../im6.github.io"
 
+if [ ! -d "$im6_dir" ]; then
+  echo 'GitHub page folder does not exist'
+  exit 1
+fi
+
 # copy
 cp dist/public/main.js "$im6_dir/assets/"
 cp dist/public/site.js "$im6_dir/assets/"
+
+cp assets/CNAME $im6_dir
+cp assets/robots.txt $im6_dir
+cp assets/sitemap.xml $im6_dir
+
 cp -a dist/views/. $im6_dir
-cp -a assets/. $im6_dir
 
 # push
 cd $im6_dir
