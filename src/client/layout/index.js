a215c0758931eedd8e6475e5c5488ae4d7c26d1e
im6 <ee2win@gmail.com>
2020-09-04 21:41:56 -0400

    fix not find dom error

--- src/client/layout/index.js
+++ src/client/layout/index.js
@@ -7,14 +7,17 @@ import {
   header,
 } from '../../components/AppContainer/Layout/components/Sidebar/style.less';
 
-const initTyped = () => {
-  const typeElem = document.getElementsByClassName(type)[0];
-  const funTxt = document.getElementsByClassName(header)[0].children[1];
-  const colorNum = leftNavTextColors.length;
+const typeElems = document.getElementsByClassName(type);
+const headerElems = document.getElementsByClassName(header);
 
+if (typeElems.length && headerElems.length) {
+  const typeElem = typeElems[0];
+  const funTxtElem = headerElems[0].children[1];
+  const colorNum = leftNavTextColors.length;
   typeElem.innerText = '';
 
-  return new Typed(`.${type}`, {
+  // eslint-disable-next-line no-new
+  new Typed(`.${type}`, {
     strings: leftNavText,
     typeSpeed: 70,
     backSpeed: 40,
@@ -22,12 +25,13 @@ const initTyped = () => {
     loop: true,
     autoInsertCss: true,
     preStringTyped: (index) => {
-      const typeCurosr = document.getElementsByClassName('typed-cursor')[0];
-      typeCurosr.style.color = leftNavTextColors[index % colorNum][0]; // eslint-disable-line prefer-destructuring
-      typeElem.style.color = leftNavTextColors[index % colorNum][0]; // eslint-disable-line prefer-destructuring
-      funTxt.style.color = leftNavTextColors[index % colorNum][1]; // eslint-disable-line prefer-destructuring
+      const selectedColor = leftNavTextColors[index % colorNum];
+      const [typeCurosr] = document.getElementsByClassName('typed-cursor');
+      typeElem.style.color = selectedColor[0]; // eslint-disable-line prefer-destructuring
+      typeCurosr.style.color = selectedColor[0]; // eslint-disable-line prefer-destructuring
+      funTxtElem.style.color = selectedColor[1]; // eslint-disable-line prefer-destructuring
     },
   });
-};
-
-initTyped();
+} else {
+  console.error('render error.'); // eslint-disable-line no-console
+}
