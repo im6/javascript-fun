d03fe521c78ad5a26c157475a4b0c10a8a5a3621
im6 <ee2win@gmail.com>
2023-05-29 11:39:25 -0400

    add ads.txt to assets

--- tasks/cp.sh
+++ tasks/cp.sh
@@ -14,9 +14,7 @@ cd ../javascript-fun
 # cp packages/javascript-fun/dist/public/main.js "$im6_dir/assets/"
 # cp packages/javascript-fun/dist/public/site.js "$im6_dir/assets/"
 
-cp assets/CNAME $im6_dir
-cp assets/robots.txt $im6_dir
-cp assets/sitemap.xml $im6_dir
+cp assets/* $im6_dir
 
 cp -a packages/javascript-fun/dist/views/. $im6_dir
 
