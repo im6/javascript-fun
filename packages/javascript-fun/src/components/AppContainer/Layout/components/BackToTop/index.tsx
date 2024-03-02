e9f71c817d5ed0a3f7b603bc621dbe8742c7edaf
im6 <ee2win@gmail.com>
2024-03-02 11:40:43 -0500

    update some components

--- packages/javascript-fun/src/components/AppContainer/Layout/components/BackToTop/index.tsx
+++ packages/javascript-fun/src/components/AppContainer/Layout/components/BackToTop/index.tsx
@@ -1,13 +1,19 @@
 import { FC } from 'react';
-import style from './style.less';
 
 interface DisqusProps {
   iconCdnUrl: string;
 }
 
 const BackToTop: FC<DisqusProps> = ({ iconCdnUrl }) => (
-  <div className={style.scrollBtn}>
-    <img src={`${iconCdnUrl}/fa-chevron-up.svg`} alt="back to top" />
+  <div
+    id="scrollToTopBtn"
+    className="fixed bottom-20 right-0 md:right-10 h-10 w-10 opacity-0 invisible text-center rounded-l md:rounded-md bg-current transition duration-300 hover:bg-green-300 cursor-pointer"
+  >
+    <img
+      src={`${iconCdnUrl}/fa-chevron-up.svg`}
+      className="inline-block w-5 h-5 mt-1"
+      alt="back to top"
+    />
   </div>
 );
 
